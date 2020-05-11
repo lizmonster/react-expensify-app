9e4f3e1dfcedcdfdd9bebcdab5f752ea242e765f
Elizabeth Bonesteel <lizmonster@Aasha.lan1>
2020-05-11 14:44:40 -0400

    Setup test db env

---
+++ src/firebase/firebase.js
@@ -0,0 +1,70 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+    // appId: "1:597985732475:web:b4ce35ee9f28aaf3babfde"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+
+export { firebase, database as default };
+
+// // child_removed
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// // child_changed
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// // child_added - fires once for existing stuff, and then once for new additions
+// database.ref('expenses').on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// // database.ref('expenses').on('value', (snapshot) => {
+// //     const expenses = [];
+// //     snapshot.forEach((childSnapshot) => {
+// //         expenses.push({
+// //             id: childSnapshot.key,
+// //             ...childSnapshot.val()
+// //         });
+// //     });
+// //     console.log(expenses);
+// // });
+
+// // database.ref('expenses')
+// //     .once('value')
+// //     .then((snapshot) => {
+// //         const expenses = [];
+// //         snapshot.forEach((childSnapshot) => {
+// //             expenses.push({
+// //                 id: childSnapshot.key,
+// //                 ...childSnapshot.val()
+// //             });
+// //         });
+// //         console.log(expenses);
+// //     });
+
+// // database.ref('notes/-M6pYH5IeM2LJKZX6YK_').remove();
+
+// // Create expenses
+// // Setup "expenses" with three items (description, note(opt), amount, createdAt)
+// // 3 push calls
+
+
+// // database.ref('expenses').push({
+// //     description: 'Art supplies',
+// //     note: 'I spend too much on art supplies',
+// //     amount: 5500,
+// //     createdAt: 1500
+// // });
---
+++ src/playground/promises.js
@@ -0,0 +1,19 @@
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        // resolve('This is my resolved data');
+        // resolve('This is my other resolved data');
+        reject('Something went wrong');
+    },1500);
+    // long-running asynchronous task
+    
+});
+
+console.log('before');
+
+promise.then((data) => {
+    console.log('1', data);
+}).catch((error) => {
+    console.log('error: ', error);
+});
+
+console.log('after');
